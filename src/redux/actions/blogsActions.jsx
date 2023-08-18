6d160a4995a8b80f808ba32e6d09d3f7e05dd223
Aman Kumar Sharma <sharmaaman4419@gmail.com>
2023-08-18 21:04:12 +0530

    minor changes

--- src/redux/actions/blogsActions.jsx
+++ src/redux/actions/blogsActions.jsx
@@ -10,15 +10,12 @@ import {
 } from "./types";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-const PROXY_URL = process.env.REACT_APP_API_PROXY_URL;
 
 export const getAllBlogs = () => {
   return async (dispatch) => {
     dispatch({ type: GET_BLOGS_REQUEST });
     try {
-      const response = await axios.get(
-        PROXY_URL + `${BASE_URL}/api/blog/blogs`
-      );
+      const response = await axios.get(`${BASE_URL}/api/blog/blogs`);
       if (response) {
         return dispatch({
           type: GET_BLOGS_SUCCESS,
@@ -36,7 +33,7 @@ export const writeBlog = (blog) => {
     dispatch({ type: WRITE_BLOG_REQUEST });
     try {
       const response = await axios.post(
-        PROXY_URL + `${BASE_URL}/api/blog/write-blog`,
+        `${BASE_URL}/api/blog/write-blog`,
         blog
       );
       if (response) {
